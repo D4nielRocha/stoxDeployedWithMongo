a73dcf8210a855f85018df84aaa3720a657f4418
Daniel Rocha <x00170715@mytudublin.ie>
2021-05-04 00:12:12 +0100

    added cors3 origin

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 // const HOST = "127.0.0.1";
 
-app.use('/*', function(req, res, next) {
+app.all('/*', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'content-Type,x-requested-with,Origin,Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
     next();
   });
 
 app.use( (req, res, next) => {
     res.setHeader('Content-type', 'application/json');
+    
     next();
 })
 
@@ -22,7 +23,7 @@ app.use(express.text());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(cors());
+app.use(cors({ credentials: false, origin: true }));
 app.options('*', cors()) // include before other routes
 
 
