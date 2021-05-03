5eee4b629600dc9c381604a77abea6fa7e4c88a5
Daniel Rocha <x00170715@mytudublin.ie>
2021-05-04 00:02:39 +0100

    added cors3 origin

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 // const HOST = "127.0.0.1";
 
-app.all('/*', function(req, res, next) {
+app.use('/*', function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'https://stoxclient.herokuapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    res.setHeader('Content-type', 'application/json');
     next();
   });
 
-app.use( (req, res, next) => {
-    res.setHeader('Content-type', 'application/json');
+// app.use( (req, res, next) => {
     
-    next();
-})
+//     next();
+// })
 
 
 app.use(express.text());
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors());
-// app.options('*', cors()) // include before other routes
+app.options('*', cors()) // include before other routes
 
 
 // app.use('/', require('./controllers/indexController'));
